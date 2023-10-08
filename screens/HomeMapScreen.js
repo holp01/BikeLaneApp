d1ec9c25350c44ba9908634431721297ae731d2e
Luís Teixeira <luis.teixeira@arquiconsult.com>
2023-10-08 23:30:22 +0100

    Timer aswell as Map show current location and follow

--- screens/HomeMapScreen.js
+++ screens/HomeMapScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { View, Button, Text } from "react-native";
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { startUserTrip, endUserTrip } from '../services/api';
 
@@ -11,7 +11,9 @@ function HomeMapScreen() {
     const [tripTime, setTripTime] = useState(0);
     const [waypoints, setWaypoints] = useState([]);
     const waypointIntervalRef = useRef(null);
+    const tripTimeIntervalRef = useRef(null);
     const [currentTripId, setCurrentTripId] = useState(0);
+    const [currentLocation, setCurrentLocation] = useState(null);
 
     useEffect(() => {
         async function fetchLocation() {
@@ -30,6 +32,7 @@ function HomeMapScreen() {
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421
             });
+            setCurrentLocation(location.coords);
         }
 
         fetchLocation();
@@ -37,11 +40,17 @@ function HomeMapScreen() {
 
     const startTrip = async () => {
         setIsTripActive(true);
+        // Set up a timer to update tripTime every second
+        const tripTimeInterval = setInterval(() => {
+            setTripTime(prevTime => prevTime + 1);
+        }, 1000);  // Every second
 
         // Initial waypoint capture
         const location = await Location.getCurrentPositionAsync({});
         const currentDateTime = new Date().toISOString();
         setWaypoints(prevWaypoints => [...prevWaypoints, { ...location.coords, timestamp: currentDateTime }]);
+
+        setCurrentLocation(location.coords);  // Set current location
 
         // Set up a timer to capture waypoints every few seconds
         const waypointInterval = setInterval(async () => {
@@ -52,6 +61,7 @@ function HomeMapScreen() {
 
         // Store the interval ID in a ref so you can clear it later
         waypointIntervalRef.current = waypointInterval;
+        tripTimeIntervalRef.current = tripTimeInterval;
 
         // Start trip in backend
         const tripId = await startUserTrip(new Date().toISOString());
@@ -62,6 +72,8 @@ function HomeMapScreen() {
         setIsTripActive(false);
 
         clearInterval(waypointIntervalRef.current); // Stop capturing waypoints
+
+        clearInterval(tripTimeIntervalRef.current); //Clear time interval
 
         // Prepare data to send to backend
         const tripData = {
@@ -89,19 +101,35 @@ function HomeMapScreen() {
         setCurrentTripId(null);
     };
 
+    const formatTime = (seconds) => {
+        const hours = Math.floor(seconds / 3600);
+        const minutes = Math.floor((seconds - (hours * 3600)) / 60);
+        const remainingSeconds = seconds % 60;
+
+        return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+    };
+
     return (
         <View style={{ flex: 1 }}>
             {initialPosition ? (
                 <>
                     <MapView
                         style={{ flex: 1 }}
-                        initialRegion={initialPosition}
-                    />
+                        region={{
+                            ...currentLocation,
+                            latitudeDelta: 0.0922,
+                            longitudeDelta: 0.0421
+                        }}
+                        showsUserLocation={true}
+                        followsUserLocation={true}
+                    >
+                    </MapView>
+
                     {isTripActive ? (
                         <>
                             <Button title="End Trip" onPress={endTrip} />
                             <Text>Distance Travelled: {distanceTravelled} km</Text>
-                            <Text>Time Elapsed: {tripTime} seconds</Text>
+                            <Text>Time Elapsed: {formatTime(tripTime)}</Text>
                         </>
                     ) : (
                         <Button title="Start Trip" onPress={startTrip} />
