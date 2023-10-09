204a3df22bc73411b7c18ec8f944776d42929b65
Luís Teixeira <luis.teixeira@arquiconsult.com>
2023-10-10 00:03:05 +0100

    Rewards screen and redeem

--- screens/RewardsScreen.js
+++ screens/RewardsScreen.js
@@ -1,12 +1,86 @@
-import react from 'react';
-import { View, Button, TextInput, Text } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { View, FlatList, Text, Image, Button, Modal } from "react-native";
 
-function RewardsScreen(){
-return(
-    <View>
-        <Text>Screen with rewards</Text>
-    </View>
-)
+import { getActiveRewards } from '../services/api';
+
+function RewardsScreen() {
+    const [rewards, setRewards] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [selectedReward, setSelectedReward] = useState(null);
+
+    useEffect(() => {
+        fetchActiveRewards();
+    }, []);
+
+    const fetchActiveRewards = async () => {
+        try {
+            const fetchedRewards = await getActiveRewards();
+            setRewards(fetchedRewards);
+            setLoading(false);
+        } catch (error) {
+            console.error('There was an error fetching the rewards', error);
+            setLoading(false);
+        }
+    };
+
+    const handleRedeemReward = async (rewardId) => {
+        try {
+          const response = await redeemReward(rewardId);
+          alert(response.Message);  // Or use a more user-friendly notification
+        } catch (error) {
+          alert('Failed to redeem the reward. Please try again.');  // Consider displaying the specific error message
+        }
+      };
+
+    const renderItem = ({ item }) => (
+        <View style={{ flexDirection: 'row', alignItems: 'center', margin: 20, borderWidth: 1, borderColor: '#e0e0e0', padding: 10, borderRadius: 10 }}>
+            <Image 
+                source={{ uri: 'https://example.com/placeholder-image-url.png' }}
+                style={{ width: 60, height: 60, borderRadius: 30 }}
+            />
+            <View style={{ marginLeft: 20 }}>
+                <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{item.name}</Text>
+                <Text style={{ marginTop: 6 }}>{item.description}</Text>
+                <Text style={{ marginTop: 4, color: 'gray' }}>{item.pointCost} Points</Text>
+                <Button title="Redeem" onPress={() => {setSelectedReward(item); setModalVisible(true);}} />
+            </View>
+        </View>
+    );
+
+    if (loading) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Loading...</Text>
+            </View>
+        );
+    }
+
+    return (
+        <>
+            <FlatList 
+                data={rewards}
+                renderItem={renderItem}
+                keyExtractor={item => item.id.toString()}
+            />
+            {selectedReward && (
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {setModalVisible(false);}}
+                >
+                    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                        <View style={{width: 300, padding: 20, backgroundColor: 'white', borderRadius: 10, alignItems: 'center'}}>
+                            <Text style={{marginBottom: 20}}>Are you sure you want to redeem {selectedReward.name} for {selectedReward.pointCost} Points?</Text>
+                            <Button title="Yes" onPress={() => handleRedeemReward(selectedReward.id)} />
+                            <Button title="No" onPress={() => setModalVisible(false)} />
+                        </View>
+                    </View>
+                </Modal>
+            )}
+        </>
+    );
 }
 
 export default RewardsScreen;
--- services/api.js
+++ services/api.js
@@ -1,56 +1,77 @@
 import axios from 'axios';
 
-const API_URL = 'https://5252-2001-818-eb85-9b00-9b0d-7f57-7155-5d2a.ngrok.io/api/';
+const API_URL = 'https://24dd-2001-818-eb85-9b00-9b0d-7f57-7155-5d2a.ngrok.io/api/';
 
 const api = axios.create({
-    baseURL: API_URL,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 
 //Authentication
-  export const setAuthToken = token => {
-    if (token) {
-      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    } else {
-      delete api.defaults.headers.common['Authorization'];
-    }
-  };
+export const setAuthToken = token => {
+  if (token) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common['Authorization'];
+  }
+};
 
-  export const loginUser = async (credentials) => {
-    try {
-      const response = await api.post('user/login', credentials);
-      return response.data;
-    } catch (error) {
-      throw error.response.data;
-    }
-  };
+export const loginUser = async (credentials) => {
+  try {
+    const response = await api.post('user/login', credentials);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
 
-  export const registerUser = async (userData) => {
-    try {
-      const response = await api.post('user/register', userData);
-      return response.data;
-    } catch (error) {
-      throw error.response.data;
-    }
-  };
-  
+export const registerUser = async (userData) => {
+  try {
+    const response = await api.post('user/register', userData);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
 //Trips
-  export const startUserTrip = async (startTime) => {
-    try {
-      const response = await api.post('trip/start', { StartTime: startTime });
-      return response.data;
-    } catch (error) {
-      throw error.response.data;
-    }
-  };
-  
-  export const endUserTrip = async (tripData) => {
-    try {
-      const response = await api.post('trip/end', tripData);
-      return response.data;
-    } catch (error) {
-      throw error.response.data;
-    }
-  };
+export const startUserTrip = async (startTime) => {
+  try {
+    const response = await api.post('trip/start', { StartTime: startTime });
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const endUserTrip = async (tripData) => {
+  try {
+    const response = await api.post('trip/end', tripData);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+//Rewards
+export const getActiveRewards = async () => {
+  try {
+    const response = await api.get('rewards/getActive');
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const redeemReward = async (rewardId) => {
+  try {
+    const response = await api.post('rewards/redeem', {
+      RewardId: rewardId,
+    });
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
