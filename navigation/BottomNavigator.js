915abec366a3a5bed1d5656859e611c43868a5c4
Luís Teixeira <luis.teixeira@arquiconsult.com>
2023-10-10 19:17:20 +0100

    Created Settings Screen

--- navigation/BottomNavigator.js
+++ navigation/BottomNavigator.js
@@ -3,26 +3,32 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeMapScreen from '../screens/HomeMapScreen';
 import RewardsScreen from '../screens/RewardsScreen';
 import HistoryScreen from '../screens/HistoryScreen';
+import SettingsScreen from '../screens/SettingsScreen';
 
 const Tab = createBottomTabNavigator();
 
 const BottomNavigator = () => {
   return (
     <Tab.Navigator initialRouteName="HomeMap">
-      <Tab.Screen 
-        name="HomeMap" 
-        component={HomeMapScreen} 
-        options={{ tabBarLabel: 'Map' }} 
+      <Tab.Screen
+        name="HomeMap"
+        component={HomeMapScreen}
+        options={{ tabBarLabel: 'Map' }}
       />
-      <Tab.Screen 
-        name="Rewards" 
-        component={RewardsScreen} 
-        options={{ tabBarLabel: 'Rewards' }} 
+      <Tab.Screen
+        name="Rewards"
+        component={RewardsScreen}
+        options={{ tabBarLabel: 'Rewards' }}
       />
-      <Tab.Screen 
-        name="History" 
-        component={HistoryScreen} 
-        options={{ tabBarLabel: 'History' }} 
+      <Tab.Screen
+        name="History"
+        component={HistoryScreen}
+        options={{ tabBarLabel: 'History' }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{ tabBarLabel: 'Settings' }}
       />
     </Tab.Navigator>
   );
